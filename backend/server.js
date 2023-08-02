76f42db170d3df3ad1b2818a41764a51031a35ef
swesadiqul <mdsadiqulislam390@gmail.com>
2023-08-02 18:12:06 +0600

    updated code on server.js file and gitignore file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
-console.log('Hello world');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+const app = express()
+
+
+app.get('/api/todos', (req, res) => {
+  res.send('Get Todos.')
+});
+
+app.post('/api/todos', (req, res) => {
+  res.send('Set Todo.')
+});
+
+app.get('/api/todos/:id', (req, res) => {
+  res.send('Get Todo.')
+});
+
+app.put('/api/todos/:id', (req, res) => {
+  res.send('Update Todo.')
+});
+
+app.delete('/api/todos/:id', (req, res) => {
+  res.send('Delete Todo.')
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
