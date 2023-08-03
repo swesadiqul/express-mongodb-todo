fae2835a608a44efdba6d91ba2ec8b21cf2ddf9e
swesadiqul <mdsadiqulislam390@gmail.com>
2023-08-03 19:28:23 +0600

    user signup and login.

--- backend/controllers/todoControlller.js
+++ backend/controllers/todoControlller.js
@@ -65,7 +65,6 @@ const deleteTodo = asyncHandler (async (req, res) => {
 })
 
 
-
 module.exports = {
     getTodos,
     addTodo,
--- backend/models/todoModel.js
+++ backend/models/todoModel.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const todoSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "This field is required."],
+    },
     title: {
       type: String,
       required: [true, "This field is required."],
